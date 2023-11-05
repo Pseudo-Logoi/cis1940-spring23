ad41976be594b8f5182f3fed0f8af8c1c374082c
Harry Wong <36160289+Pseudo-Logoi@users.noreply.github.com>
2023-11-05 04:30:15 +0000

    finish week02 class

--- week02/class/Exercises.hs
+++ week02/class/Exercises.hs
@@ -41,10 +41,36 @@ tree = Node Leaf 1 (Node Leaf 2 Leaf)
 
 {- Count the number of leaves in the tree. For example,
    [numLeaves tree] should be [3]. -}
+numLeaves :: Tree -> Int
+numLeaves Leaf = 1
+numLeaves (Node l_child _ r_child) = numLeaves l_child + numLeaves r_child
 
 {- Check whether a number is in the tree. For example,
    [find 1 tree] and [find 2 tree] should be [True], while
    [find 3 tree] should be [False]. -}
+find :: Int -> Tree -> Bool
+find val Leaf = False
+find val (Node l_child root_val r_child) = val == root_val || find val l_child || find val r_child
 
 {- Add three to every node value in the tree. For example,
    [add3 tree] should be [Node Leaf 4 (Node Leaf 5 Leaf)]. -}
+add3 :: Tree -> Tree
+add3 Leaf = Leaf
+add3 (Node l_child root_val r_child) = Node (add3 l_child) (root_val + 3) (add3 r_child)
+
+data Thing
+  = Shoe
+  | Ship
+  | SealingWax
+  | Cabbage
+  | King
+  deriving (Show)
+
+data Person = Person String Int Thing
+  deriving (Show)
+
+brent :: Person
+brent = Person "Brent" 31 SealingWax
+
+displayPerson :: Person -> String
+displayPerson p@(Person n _ _) = "The name field of (" ++ show p ++ ") is " ++ n
